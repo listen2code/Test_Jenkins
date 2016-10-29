517ae1acc352e07f458b7bc345b9495dcbc2904b
李松 <lisong@dingdatech.com>
2016-10-29 20:02:21 +0800

    build

--- app/src/main/java/com/listen/test_jenkins/MainActivity.java
+++ app/src/main/java/com/listen/test_jenkins/MainActivity.java
@@ -1,13 +1,28 @@
 package com.listen.test_jenkins;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTvChannel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTvChannel = ((TextView) findViewById(R.id.tv_channel));
+
+        ActivityInfo info = null;
+        try {
+            info = getPackageManager().getActivityInfo(getComponentName(), PackageManager.GET_META_DATA);
+            mTvChannel.setText(info.metaData.getString("CHANNEL"));
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
